fcf541718e0626efd70c25dc235f751e11fa3f3c
itouri <hechima.itouri@gmail.com>
2018-03-18 17:57:06 +0900

    Added routing

--- client/reversi/src/app/app-routing.module.ts
+++ client/reversi/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
+import { RoomsComponent } from './rooms/rooms.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/rooms', pathMatch: 'full' },
+  { path: 'rooms', component: RoomsComponent },
+];
 
 @NgModule({
   imports: [
+    RouterModule.forRoot(routes),
     CommonModule
   ],
+  exports: [ RouterModule ],
   declarations: []
 })
 export class AppRoutingModule { }
